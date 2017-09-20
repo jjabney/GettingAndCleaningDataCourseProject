0c5706ead552c7a6e0c6a19c0e3e112e915f1fab
James Abney <jjabney@gmail.com>
2017-09-20 10:36:54 -0700

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,10 +1,11 @@
+
 run_analysis<- function()
 {
-
+  
   library("dplyr")
   library("stringr")
   library("data.table")
-
+  
   fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
   
   destFile <- "UCI_HAR_Dataset.zip"
@@ -12,17 +13,16 @@ run_analysis<- function()
   zip <-  download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip","FUCIHARDataset.zip")
   
   unzip(destFile)
-
+  
   #load feature names
   featNames <- read.table("~/UCI HAR Dataset/features.txt")
-
   
-  View(featNames)
+
   
   #load activity labels
   actLabels <- read.table("~/UCI HAR Dataset/activity_labels.txt", header = FALSE)
   
-  
+
   #load training data
   subjTrng <- read.table("~/UCI HAR Dataset/train/subject_train.txt", header = FALSE)
   actTrng <- read.table("~/UCI HAR Dataset/train/y_train.txt", header = FALSE)
@@ -41,8 +41,10 @@ run_analysis<- function()
   #name columns
   colnames(feat) <- t(featNames[2])
   colnames(act) <- "Activity"
-  colnames(subj) <- "Subject"
+  
 
+  colnames(subj) <- "Subject"
+  
   #construt observations
   obs <- cbind(subj,act,feat)
   
@@ -59,10 +61,10 @@ run_analysis<- function()
   names(obs)<-gsub("-freq()", "Frequency", names(obs), ignore.case = TRUE)
   names(obs)<-gsub("angle", "Angle", names(obs))
   names(obs)<-gsub("gravity", "Gravity", names(obs))
-
+  
   #reduce observation columns to only the activity, subject, standard deviation & mean values
   obs <- obs[grep("(activity|subject|std|mean)",names(obs),ignore.case = TRUE)]
-
+  
   #tidy data set average for each activity and subject.
   tidyData <- aggregate(. ~Subject + Activity, obs, mean)
   
@@ -71,7 +73,7 @@ run_analysis<- function()
   
   #write to file
   write.table(tidyData, file = "Tidy.txt", row.names = FALSE)
- 
+  
 
 }
 
